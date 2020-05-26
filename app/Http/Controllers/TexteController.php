d86dbe7870baa059becce6a633784980c412034c
stephane65133 <stephanejiofatamafo@gmail.com>
2020-05-26 10:51:23 +0100

    initial commit

---
+++ app/Http/Controllers/TexteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\texte;
+use Illuminate\Http\Request;
+
+class TexteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\texte  $texte
+     * @return \Illuminate\Http\Response
+     */
+    public function show(texte $texte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\texte  $texte
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(texte $texte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\texte  $texte
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, texte $texte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\texte  $texte
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(texte $texte)
+    {
+        //
+    }
+}
